c546a8d01e00e331bb3f054891a8015016ca7e27
Nathan Phennel <nathanphennel@gmail.com>
2018-06-15 15:58:50 +1000

    learnin: handle text input

--- App.js
+++ App.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
-import { Image, View, Text } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { AppRegistry, Text, View, TextInput } from "react-native";
 
-
-class Greeting extends Component {
-  render() {
-    return (
-      <Text>Hello {this.props.name}!</Text>
-    );
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
   }
-}
 
-export default class Bananas extends Component {
   render() {
-    let pic = {
-      uri: 'https://media.licdn.com/dms/image/C5103AQFkB6B9cYYAXA/profile-displayphoto-shrink_200_200/0?e=1529607600&v=beta&t=sYbINtA1eZLF4KFlPesyj1GLt-2w9isJszM-Nl5m4BA'
-    };
     return (
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Greeting name='Hanna' style={{ zIndex: 10 }} />
-        <Image source={pic} style={{ width: 293, height: 510, zIndex: -10 }} />
-        <Text> Don't you look great! </Text>
+      <View style={{ padding: 100 }}>
+        <TextInput
+          style={{ height: 70 }}
+          placeholder="Type text here"
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text style={{ padding: 20, fontSize: 45 }}>
+          {this.state.text}
+        </Text>
       </View>
-    );
+    )
   }
 }
