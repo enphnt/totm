34df42ea693329d4ac5722025cacf6da94625466
Nathan Phennel <nathanphennel@gmail.com>
2018-07-24 01:59:35 +1000

    feat: added some data for pre-populated search terms

---
+++ src/data/search-terms.js
@@ -0,0 +1,41 @@
+// My initial idea is that if a user wants, they can optionally use one
+// of our pre-defined 'curated' search terms. These are vetted by doing
+// a live test of autocomplete against the term:
+
+module.exports = [{
+  term: "why isn't ",
+}, {
+  term: "i am extremely terrified of ",
+}, {
+  term: "is it better to have ",
+}, {
+  term: "how do i tell my ",
+}, {
+  term: "how can i be sure that ",
+}, {
+  term: "on what day did ",
+}, {
+  term: "why is there a ",
+}, {
+  term: "why do i always ",
+}, {
+  term: "why don't i ever ",
+}, {
+  term: "why don't we ever ",
+}, {
+  term: "what is it called when ",
+}, {
+  term: "why do people ",
+}, {
+  term: "why don't people ",
+}, {
+  term: "don't you hate it when a ",
+}, {
+  term: "never put a ",
+}, {
+  term: "what happens if i take ",
+}, {
+  term: "what happens if i don't ",
+}, {
+  term: "why don't people ",
+}];
